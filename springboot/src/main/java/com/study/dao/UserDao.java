e188acd5201f3f0110b5a1cbf1e4c6e7ff9ea116
99171664 <99171664@qq.com>
2018-08-24 16:52:40 +0800

    添加注释

--- springboot/src/main/java/com/study/dao/UserDao.java
+++ springboot/src/main/java/com/study/dao/UserDao.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * @author zyq
+ * CrudRepository是增删改查的基本接口,泛型是查询的Entity和Entity主键类型
+ * JpaSpecificationExecutor是复杂查询
+ *
+ */
 @Repository
 public interface UserDao extends CrudRepository<User,Long>, JpaSpecificationExecutor<User> {
     /**
@@ -14,8 +20,14 @@ public interface UserDao extends CrudRepository<User,Long>, JpaSpecificationExec
      * @param age
      * @return
      */
+
     List<User> findUserByAge(Integer age);
 
+    /**
+     * 根据名称获取列表
+     * @param name
+     * @return
+     */
     List<User> findAllByNameEquals(String name);
 
 }
